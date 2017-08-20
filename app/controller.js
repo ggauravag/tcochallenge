d1b1cffa3b4c544f8942b616faa8bda84a207fed
Gaurav Agarwal <gagarwal@ptc.com>
2017-08-20 16:07:17 +0530

    Level 3

--- app/controller.js
+++ app/controller.js
@@ -1,4 +1,6 @@
 const dropletUtils = require('./droplet-utils');
+const request = require('request');
+const digitalocean = require('digitalocean');
 
 exports.getDropletDetails = function (req, res) {
     dropletUtils.getDropletDetails(function (err, details) {
@@ -12,4 +14,25 @@ exports.getDropletDetails = function (req, res) {
 
 exports.getHandleName = function (req, res) {
     res.json({handle: 'ggauravag'});
+};
+
+exports.getClientDropletDetails = function (req, res) {
+    var accessCode = req.code;
+    request({
+        uri: 'https://cloud.digitalocean.com/v1/oauth/token?client_id=61de28bfd1b5782f984fdda0fdcdd500b6908359c2c75eb53b064674fbb9a613&client_secret=423a852d336681849b03ab0a77eed3412af2e0da93ffbabe329943d31f732b30&grant_type=authorization_code&code=' + accessCode + '&redirect_uri=http://139.59.0.169/oauth-handler'
+    }, function (err, res, body) {
+        if (err) {
+            res.status(500).json(err);
+        } else if (res.statusCode >= 200 && res.statusCode < 300) {
+            var details = JSON.parse(body);
+            var accessToken = details.access_token;
+            var client = digitalocean.client(accessToken);
+            client.droplets.list().then(function(droplets) {
+                var droplet = droplets[0];
+                res.json(droplet);
+            })
+        } else {
+            res.status(500).json(body);
+        }
+    })
 };
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-var controller = require('./app/controller')
+var controller = require('./app/controller');
+var path       = require('path');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'client')));
 
 var port = process.env.PORT || 8080;        // set our port
 
@@ -17,12 +21,17 @@ var router = express.Router();              // get an instance of the express Ro
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/handle', controller.getHandleName);
 router.get('/droplet-details', controller.getDropletDetails);
+router.get('/oauth-handler', controller.getDropletDetails);
 
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/index.html'));
+});
 
 // START THE SERVER
 // =============================================================================
